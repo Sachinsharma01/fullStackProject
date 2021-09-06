1fcd22f5bc276af7e2fbdeb33e0432e797522f8f
Vipin Chaudhary <vipin.chaudhary_cs19@gla.ac.in>
2021-09-06 22:25:03 +0530

    added login page without login fucntionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,22 @@
+var pass = document.getElementById('showPass')
+var toggle = document.getElementById('togglePass')
+var password = document.getElementById('password')
+
 document.getElementById('hamburger').onclick = function () {
   document.getElementById('ham-menu').classList.toggle('active')
   this.classList.toggle('active')
+  if (toggle.style.zIndex == 0) {
+    toggle.style.zIndex = -1
+  } else {
+    toggle.style.zIndex = 0
+  }
+}
+
+pass.onclick = function () {
+  toggle.classList.toggle('active')
+  if (password.type == 'text') {
+    password.type = 'password'
+  } else {
+    password.type = 'text'
+  }
 }
